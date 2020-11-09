e31e6bb60fd81a3e29bad45dd33e44d6c5b1a181
Hugo Martiniano <hugomartiniano@gmail.com>
2020-11-09 01:16:34 +0000

    Fixed timestamp convertion to datetime.

--- graph.py
+++ graph.py
@@ -3,7 +3,8 @@ import glob
 fnames = glob.glob("data/*json")
 import json
 df = pd.DataFrame([json.load(open(fname)) for fname in fnames])
-df["data"] = pd.to_datetime(df.dia.astype(str) + "-" + df.mes.astype(str) + "-" + df.ano.astype(str))
+df["data"] = pd.to_datetime(df.dia.astype(str) + "-" + df.mes.astype(str) + "-"
+                            + df.ano.astype(str), format='%d-%m-%Y',)
 df = df.sort_values(by="data")
 print(df.head())
 import seaborn as sns
@@ -14,5 +15,8 @@ df.value = df.value.astype(int)
 print(df.head())
 print(df.dtypes)
 lp = sns.lineplot(data=df, x="data", y="value", hue="variable")
+plt.xticks(rotation=45)
+plt.tight_layout()
 plt.savefig('time-series.png', dpi=300)
+
 
