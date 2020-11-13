a0393b465af3e4ee5f2e59ad43f384f6ad181831
Hugo Martiniano <hugomartiniano@gmail.com>
2020-11-13 12:12:24 +0000

    Cleanup graph code.

--- graph.py
+++ graph.py
@@ -1,16 +1,21 @@
-import pandas as pd
 import glob
-fnames = glob.glob("data/*json")
 import json
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+fnames = glob.glob("data/*json")
 df = pd.DataFrame([json.load(open(fname)) for fname in fnames])
 df["data"] = pd.to_datetime(df.dia.astype(str) + "-" + df.mes.astype(str) + "-"
                             + df.ano.astype(str), format='%d-%m-%Y',)
 df = df.sort_values(by="data")
 print(df.head())
-import seaborn as sns
-import matplotlib.pyplot as plt
+#df["por 100 mil hab."] = df["ativos"].astype(int) / 13647 * 100000
 df = df.drop(columns=["dia", "mes", "ano"])
-df = pd.melt(df, id_vars="data", value_vars=["ativos", "recuperados", "obitos"],
+df = pd.melt(df, id_vars="data",
+             #value_vars=["ativos", "recuperados", "obitos", "por 100 mil hab."],
+             value_vars=["ativos", "recuperados", "obitos"],
              value_name="casos", var_name="tipo")
 df.casos = df.casos.astype(int)
 print(df.head())
